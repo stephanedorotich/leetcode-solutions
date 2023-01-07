21aba2776f25685d9d4f741ccf706cf3becf902c
Stephane Dorotich <stephanedorotich@gmail.com>
2023-01-07 14:05:41 -0700

    Problem: Longest Palindromic Substring 	Implemented O(n) solution. Now beats 96% of submissions. 	Scans for "centers" of palindromes that are longer than the previous longest palindrome. 	Once found, expand outward from the center as much as possible.

--- medium/5-Longest-Palindromic-Substring.cpp
+++ medium/5-Longest-Palindromic-Substring.cpp
@@ -6,7 +6,7 @@
 
 class Solution {
 private:
-    bool isPalindrome(std::string & s, uint16_t i, uint16_t j) {
+    bool isPalindrome(const std::string & s, uint16_t i, uint16_t j) {
         while (i <= j) {
             if (s[i] != s[j]) return false;
             i++;
@@ -15,16 +15,31 @@ private:
         return true;
     }
 
+    void expandPalindrome(const std::string & s, uint16_t & i, uint16_t & j) {
+        while (i > 0 && j < s.length()-1 && s[i-1] == s[j+1]) {
+            i--;
+            j++;
+        }
+    }
+
 public:
-    std::string longestPalindrome(std::string s) {
-        uint16_t max_len = 1, pos = 0;
+    std::string longestPalindrome(std::string & s) {
+        uint16_t max_len = 1, pos = 0, x, y;
         for (uint16_t i = 0; i < s.length(); i++) {
-            for (uint16_t j = i+max_len; j < s.length(); j++) {
-                if (s[j] == s[i] && isPalindrome(s, i, j)) {
-                    pos = i;
-                    max_len = j-i+1;
-                    std::cout << "Pal: " << s.substr(pos, max_len) << std::endl;
-                }
+            if (i+max_len < s.length() && s[i] == s[i+max_len] && isPalindrome(s, i+1, i+max_len-1)) {
+                x = i;
+                y = i+max_len;
+                expandPalindrome(s, x, y);
+                pos = x;
+                max_len = y-x+1;
+                i--;
+            } else if (i+max_len+1 < s.length() && s[i] == s[i+max_len+1] && isPalindrome(s, i+1, i+max_len)) {
+                x = i;
+                y = i+max_len+1;
+                expandPalindrome(s, x, y);
+                pos = x;
+                max_len = y-x+1;
+                i--;
             }
         }
         return s.substr(pos, max_len);
@@ -37,10 +52,10 @@ void usage_error(const char * progName) {
 }
 
 int main(int argc, char ** argv) {
-    // if (argc != 2) usage_error(argv[0]);
+    if (argc != 2) usage_error(argv[0]);
 
+    std::string input = argv[1];
     Solution s;
-    std::string input = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
     std::string res = s.longestPalindrome(input);
     std::cout << "Longest Palindromic Substring: " << res << std::endl;
     return 0;
